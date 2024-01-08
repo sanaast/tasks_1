b516da91ba029bc2dc2dfbb1a6ccc6052b3f4c44
sanaa tizi <sanaatizi8@gmail.com>
2024-01-08 23:15:35 +0100

    initial commit

---
+++ tasks.js
@@ -0,0 +1,111 @@
+//BASIC
+
+//task1
+let firstname="sanaa";
+let lastname="tizi";
+const pi=3.14;
+let radius=8;
+let favoriteSuperCase="Ahmad shuguairi";
+let favoriteQuote="TOGETHER WE STUND,DIVIDED WE FALL";
+
+// task2
+// fullName
+let fullname=firstname+" "+lastname;
+
+//maths
+let area =pi*Math.pow(radius,2);
+let perimeter=2 * pi*radius;
+
+//motivation
+let motivation="a wise man named "+favoriteSuperCase+" says :"+favoriteQuote;
+
+console.log("FullName is: "+fullname);
+console.log("value the area of cercle is: "+area+" and value the  perimeter of cercle is: "+perimeter)
+console.log("Motivation:"+motivation)
+
+//task3
+// swap the values
+let a=3;
+let b=10;
+
+let c=a;
+a=b;
+b=c;
+console.log("swapping a ="+a+" and b="+b);
+Z
+//********************************************************** */
+
+// Conditional Statements
+
+//task1
+//EVEN OR ODD
+let num=2
+if (num%2==0){
+    console.log(num + " is even number");
+    }else{
+        console.log(num +" is odd number")
+    } ;
+
+//task2
+//DAYS OF THE WEEK
+let day=4;
+switch (day){
+    case 1:
+    console.log("Monday");
+    break;
+    case 2:
+    console.log("Tusday");
+    break;
+    case 4:
+    console.log("Wednesday");
+    break;
+    case 5:
+    console.log("Tursday");
+    break;
+    case 6:
+    console.log("Friday");
+    break;
+    case 7:
+    console.log("Saturday");
+    break;
+    case 8:
+    console.log("Sunday");
+    break;
+    default:
+        console.log("Invalid Day");
+}
+//task3
+//maximum 
+let d =-15;
+let e=6;
+let f=2.6;
+
+let max="le maxumum is "+Math.max(d,e,f);
+console.log(max);
+
+//task4
+// THE TEACHER
+let score =83
+if (score>85){
+    console.log("A");
+}else if (score>70){
+    console.log("B");
+}else if (score>55){
+    console.log("C");
+}else if (score>40){
+    console.log("D");
+}else if (score>15){
+    console.log("E");
+}else {
+    console.log("F");
+}
+
+
+
+
+
+
+
+
+
+
